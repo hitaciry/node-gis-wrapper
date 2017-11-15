42d02fa947307cb767d30af2cc6eb0466643921c
andrey <a.sh09@mail.ru>
2017-11-15 23:37:41 +0530

    add bot

--- dist/index.js
+++ dist/index.js
@@ -8,14 +8,44 @@ var _wrapper = require('./wrapper');
 
 var _wrapper2 = _interopRequireDefault(_wrapper);
 
+var _nodeTelegramBotApi = require('node-telegram-bot-api');
+
+var _nodeTelegramBotApi2 = _interopRequireDefault(_nodeTelegramBotApi);
+
+var _timers = require('core-js/library/web/timers');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
-
 app.post("/token", function (req, res) {
-  expa(req.body.email, req.body.password).getNewToken().then(res.send).catch(res.send);
+  (0, _wrapper2.default)(req.body.email, req.body.password).getNewToken().then(res.send).catch(res.send);
 });
-
 app.get("/", function (req, res) {
   return res.send("I'm working");
+});
+
+var login = 'a.shitikov90@gmail.com';
+var password = 'hitaciry90';
+var token = '457320898:AAF5Zv-Bw_rm2GHOdo2tyjcWv1etCU0NUTs';
+// Включить опрос сервера
+var bot = new _nodeTelegramBotApi2.default(token, { polling: true });
+
+// Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием.)
+bot.onText(/\/getNew/, function (msg, match) {
+  var fromId = msg.from.id;
+  var date = new _timers.Date();
+  var resp = (0, _wrapper2.default)(login, password).post('people.json', { "filters[home_committee]": 1618,
+    "filters[registered[from]]": date }).then(function (data) {
+    console.log(data);
+  });
+  bot.sendMessage(fromId, 'Im work');
+});
+bot.onText(/\/start/, function (msg) {
+
+  bot.sendMessage(msg.chat.id, "Welcome");
+});
+bot.onText(/\/hello/, function (msg, match) {
+  var fromId = msg.from.id;
+  var resp = 'hello';
+  bot.sendMessage(fromId, resp);
 });
--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 import express  from 'express'
-import EXPA from "./wrapper"
+import EXPA from './wrapper'
+import TelegramBot from 'node-telegram-bot-api'
+import { Date } from 'core-js/library/web/timers';
 
 const app= express();
-
 app.post("/token", function (req, res) {
-  expa(req.body.email,req.body.password).getNewToken().then(res.send).catch(res.send)
+  EXPA(req.body.email,req.body.password).getNewToken().then(res.send).catch(res.send)
 })
-
 app.get("/",(req, res)=>res.send("I'm working"))
+
+const login = 'a.shitikov90@gmail.com'
+const password = 'hitaciry90'
+const token = '457320898:AAF5Zv-Bw_rm2GHOdo2tyjcWv1etCU0NUTs';
+// Включить опрос сервера
+const bot = new TelegramBot(token, {polling: true});
+
+// Написать мне ... (/echo Hello World! - пришлет сообщение с этим приветствием.)
+bot.onText(/\/getNew/, function (msg, match) {
+    const fromId = msg.from.id;
+    const date = new Date()
+    const resp = EXPA(login,password).post('people.json',{"filters[home_committee]":1618,
+      "filters[registered[from]]":date}).then(data=>{console.log(data);})
+    bot.sendMessage(fromId, 'Im work' );
+});
+bot.onText(/\/start/, (msg) => {
+  
+  bot.sendMessage(msg.chat.id, "Welcome");
+      
+  });
+bot.onText(/\/hello/, function (msg, match) {
+  const fromId = msg.from.id;
+  const resp = 'hello';
+  bot.sendMessage(fromId, resp);
+});
