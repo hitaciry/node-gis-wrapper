2496c588bb2a0b5ca462ba908e4ec02c89b5c441
indianic <indianic@IND609>
2017-11-22 11:29:52 +0530

    fix express part

--- dist/index.js
+++ dist/index.js
@@ -8,12 +8,33 @@ var _wrapper = require('./wrapper');
 
 var _wrapper2 = _interopRequireDefault(_wrapper);
 
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
-app.post("/token", function (req, res) {
-  (0, _wrapper2.default)(req.body.email, req.body.password).getNewToken().then(res.send).catch(res.send);
+
+app.use(_bodyParser2.default.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
+
+app.post("/token", _bodyParser2.default.urlencoded({ extended: true }), function (req, res, next) {
+  console.log(req.headers);
+  (0, _wrapper2.default)(req.body.email, req.body.password).getToken().then(function (t) {
+    console.log(t);res.send(t);next();
+  }).catch(function (e) {
+    console.log(e);res.send(e);
+  });
+});
+
 app.get("/", function (req, res) {
   return res.send("I'm working");
 });
--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import express  from 'express'
-import EXPA from './wrapper'
+import EXPA from "./wrapper"
+import bodyParser from 'body-parser'
 
 const app= express();
-app.post("/token", function (req, res) {
-  EXPA(req.body.email,req.body.password).getNewToken().then(res.send).catch(res.send)
-})
-app.get("/",(req, res)=>res.send("I'm working"))
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+})
+
+app.post("/token",bodyParser.urlencoded({ extended: true }), function (req, res, next) {
+  console.log(req.headers)
+  EXPA(req.body.email,req.body.password).getToken()
+  .then(t=>{console.log(t);res.send(t);next()})
+  .catch(e=>{console.log(e);res.send(e)})
+})
+
+app.get("/",(req, res)=>res.send("I'm working"))
